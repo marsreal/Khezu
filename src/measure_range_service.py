76de5a3ce89050c6618c3e9a286eaede22a1b08a
marsreal <mrealvial@gmail.com>
2022-10-11 16:10:57 +0200

    measured_range update

--- src/measure_range_service.py
+++ src/measure_range_service.py
@@ -3,11 +3,15 @@
 import rospy
 import numpy as np
 import sys 
-from khezu.srv import MeasureRange
+from stalker.srv import MeasureRange
 from utils import netcat
 from cola2_msgs.msg import NavSts
-from khezu.msg import Range
+from stalker.msg import Range
 import utm
+from threading import Thread
+from threading import Semaphore
+import time
+from cola2_ros import param_loader
 
 
 HOSTNAME1 = '10.42.57.1'
@@ -21,13 +25,17 @@ class SlantRange:
     def __init__(self,debug,sim,interface):
         # Creates a node with name 'measure_range' and make sure it is a
         # unique node (using anonymous=True).
+        self.mod_id = []
         rospy.init_node('slant_range', anonymous=True)
         self.name = rospy.get_name()
         namespace = rospy.get_namespace()
+        self.get_config()
+
+
         self.pub_range_info = rospy.Publisher(namespace + "range_info",
 										Range, queue_size=1)
         # A service
-        self.srv = rospy.Service('khezu/measure_range', MeasureRange,
+        self.srv = rospy.Service('stalker/measure_range', MeasureRange,
         					self.measure_range_callback)
         
         # A subscriber to the topic '/sparus2/navigator/navigation'. self.update_pose is called
@@ -44,34 +52,50 @@ class SlantRange:
         self.auv_y = 0
         self.auv_z = 0
         self.pose = NavSts()
+        self.flag = 0
+        self.smph = Semaphore(1)
+        rospy.Timer(rospy.Duration(5), self.range_info_callback)
+
         
-        
+    def range_info_callback(self,event):
+        #modem_id = [1,2]
+        for i in self.mod_id:
+            self.smph.acquire()
+            try:
+                slant_range = self.modem.slant_range(i)
+            except: 
+                slant_range = -1
+                print('WARNING: PORT READ ERROR, RECURSIVE ACCESS')
+            self.smph.release()
+            rangeinfo = Range()
+            rangeinfo.id = i
+            rangeinfo.range = slant_range
+            rangeinfo.header.frame_id = "sparus2/modem"
+            rangeinfo.header.stamp = rospy.Time().now()
+            self.pub_range_info.publish(rangeinfo)
+
+
     def measure_range_callback(self, request):
     	#send a message to the remote acoustic modem to compute the time of flight and obtain the slant range
     	#slant_range =  100. #this is fake, used only for debuging purposes
+        self.flag = request.modem_id
+        self.smph.acquire(timeout=2)
         if self.sim_modem_mov == True:
             self.modem.move(self.auv_x,self.auv_y,self.auv_z)
         slant_range = self.modem.slant_range(request.modem_id)
-
-        rangeinfo = Range()
-        rangeinfo.id = request.modem_id
-        rangeinfo.range = slant_range
-        rangeinfo.position.depth = self.auv_z
-        rangeinfo.global_position.latitude = self.lat_global
-        rangeinfo.global_position.longitude = self.lon_global
-        rangeinfo.header.stamp = rospy.Time().now()
-        self.pub_range_info.publish(rangeinfo)
+        self.smph.release()
+        
         return slant_range
     
     def update_pose(self, data):
         """Callback function which is called when a new message of type NavSts is
         received by the subscriber."""
         self.pose = data
-        self.lat_global = self.pose.global_position.latitude
-        self.lon_global = self.pose.global_position.longitude
+        lat = self.pose.global_position.latitude
+        lon = self.pose.global_position.longitude
         self.auv_z = self.pose.position.depth
         #AUV current position in UTM format
-        tuple = utm.from_latlon(self.lat_global, self.lon_global)
+        tuple = utm.from_latlon(lat, lon)
         easting, northing, zonenumber, zoneletter = tuple
         #origin
         lat = self.pose.origin.latitude
@@ -84,7 +108,14 @@ class SlantRange:
         self.auv_y = northing-origin_northing
         return
 
-   
+    def get_config(self):
+        """ Read parameters from ROS Param Server """
+
+        param_dict = {'mod_id': ('mod_id', [1,2])}
+
+        param_loader.get_ros_params(self, param_dict)
+    
+
 if __name__ == '__main__':
     try:
         try:
